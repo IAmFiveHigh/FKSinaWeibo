9d98056eb635081defc96aea8e12c075d51fc900
我是五高你敢信 <745170149@qq.com>
2017-04-08 18:50:38 +0800

    添加tabbar

--- FKSinaWeibo/Classes/View/Main/MainViewController.swift
+++ FKSinaWeibo/Classes/View/Main/MainViewController.swift
@@ -13,23 +13,63 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupChildControllers()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: 设置所有子控制器
+    fileprivate func setupChildControllers() {
+        
+        let array = [["clsName": "HomeViewController", "title": "首页", "imageName": ""],
+                     ["clsName": "MessageViewController", "title": "消息", "imageName": ""],
+                     ["clsName": "DiscoverViewController", "title": "发现", "imageName": ""],
+                     ["clsName": "ProfileViewController", "title": "我", "imageName": ""]
+                     ]
+        
+        var ViewControllerArray = [UIViewController]()
+        
+        //遍历数组创建控制器并保存到新数组
+        for dict in array {
+            
+            ViewControllerArray.append(controller(dict: dict))
+        }
+        
+        //设置tabbarViewController的viewControllers
+        viewControllers = ViewControllerArray
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: 使用字典创建子控制器
+    fileprivate func controller(dict: [String: String]) -> UIViewController {
+        
+        //1 取得字典内容
+        guard let clsName = dict["clsName"] else {
+            return UIViewController()
+        }
+        
+        guard let title = dict["title"] else {
+            return UIViewController()
+        }
+        
+        guard let imageName = dict["imageName"] else {
+            return UIViewController()
+        }
+        
+        let cls = NSClassFromString(Bundle.main.nameSpace + "." + clsName) as? UIViewController.Type
+        
+        let vc = cls?.init()
+        
+        vc?.title = title
+        
+        let nav = NavigationController(rootViewController: vc!)
+        
+        return nav
     }
-    */
 
+}
+
+extension Bundle {
+    
+    var nameSpace: String {
+        return infoDictionary?["CFBundleName"] as? String ?? ""
+    }
 }

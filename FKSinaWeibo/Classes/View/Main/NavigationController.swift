679a6cc15b4ff37735d86a1949c26563b0ca9544
我是五高你敢信 <745170149@qq.com>
2017-04-10 19:31:50 +0800

    设置navigation push隐藏底部tabbar

--- FKSinaWeibo/Classes/View/Main/NavigationController.swift
+++ FKSinaWeibo/Classes/View/Main/NavigationController.swift
@@ -10,26 +10,15 @@ import UIKit
 
 class NavigationController: UINavigationController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        
+        //push隐藏隐藏tabbar
+        if childViewControllers.count > 0 {
+            
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        
+        super.pushViewController(viewController, animated: animated)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
